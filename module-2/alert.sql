baf281911f9c55279608ddf259b9d8f0454c5c3d
Linus Yang <linusyoungrice@gmail.com>
2025-08-07 20:33:10 +1000

    change test

--- module-2/alert.sql
+++ module-2/alert.sql
@@ -11,9 +11,9 @@ CREATE TABLE staging_tasty_bytes.telemetry.data_quality_alerts (
 );
 
 -- Create a serverless alert with a schedule:
-
+create or replace alter order_data_quality_alert
   
-  SCHEDULE = 
+  SCHEDULE = '30 MINUTES'
   IF (EXISTS (
     SELECT * FROM STAGING_TASTY_BYTES.RAW_POS.ORDER_HEADER 
     WHERE (ORDER_AMOUNT IS NULL OR ORDER_TOTAL IS NULL) 
@@ -34,7 +34,8 @@ CREATE TABLE staging_tasty_bytes.telemetry.data_quality_alerts (
       AND ORDER_TS > DATEADD(hour, -6, CURRENT_TIMESTAMP());
         
       -- Call stored procedure for notification:
-      
+      -- call staging_tasty_bytes.raw_pos.notify_data_quality_team();
+
     END;
 
 -- Check alert status
--- module-2/solutions/solution_sproc_logs.sql
+++ module-2/solutions/solution_sproc_logs.sql
@@ -6,7 +6,7 @@ USE SCHEMA raw_pos;
 ALTER ACCOUNT SET LOG_LEVEL = 'INFO';
 
 -- Configure traces:
-
+ALTER SESSION SET TRACE_LEVEL = ALWAYS;
 
 -- Create the stored procedure, define its logic with Snowpark for Python, write sales to raw_pos.daily_sales_hamburg_t
 CREATE OR REPLACE PROCEDURE staging_tasty_bytes.raw_pos.process_order_headers_stream()
@@ -14,7 +14,7 @@ CREATE OR REPLACE PROCEDURE staging_tasty_bytes.raw_pos.process_order_headers_st
   LANGUAGE PYTHON
   RUNTIME_VERSION = '3.10'
   HANDLER ='process_order_headers_stream'
-  PACKAGES = ('snowflake-snowpark-python')
+  PACKAGES = ('snowflake-snowpark-python', 'snowflake-telemetry-python')
 AS
 $$
 import snowflake.snowpark.functions as F
@@ -34,18 +34,21 @@ def process_order_headers_stream(session: Session) -> float:
     logger.info("Starting process_order_headers_stream procedure")
 
     # Set initial span attributes for the entire procedure:
+    telemetry.set_span_attribute("procedure", "process_order_headers_stream")
 
     telemetry.set_span_attribute("trace_id", trace_id)
     
     try:
         # Begin stream query span:
-
+        telemetry.set_span_attribute("process_step", "query_stream")
+        telemetry.add_event("query_begin",{"decription": "Starting to query order_header_stream"})
 
         # Query the stream
         logger.info("Querying order_header_stream for new records")
         recent_orders = session.table("order_header_stream").filter(F.col("METADATA$ACTION") == "INSERT")
 
         # Record query completion event:
+        telemetry.add_event("query_begin",{"decription": "Completed query order_header_stream"})
 
         # Begin location filtering span
         telemetry.set_span_attribute("process_step", "filter_locations")
